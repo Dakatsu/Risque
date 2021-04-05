12b251f275a1c085b7b3ad1f83909565888cae94
Kyle Taylor Lange <dakatsu@gmail.com>
2021-04-05 02:26:23 -0400

    InputHandler uses reflection to automate command line functions.

--- risque/src/main/console/InputHandler.java
+++ risque/src/main/console/InputHandler.java
@@ -3,6 +3,9 @@ package main.console;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
 
 /**
  * Separate thread class to allow us to input data to the console while also receiving data.
@@ -10,7 +13,20 @@ import java.io.InputStreamReader;
  *
  */
 public class InputHandler extends Thread {
+	/**
+	 * The console class that manages this InputHandler.
+	 */
 	private Console d_owner;
+	
+	/**
+	 * The list of methods retrieved from the controller.
+	 */
+	private Method[] d_controllerMethods;
+	
+	/**
+	 * A map of different names for methods.
+	 */
+	private HashMap<String, String> d_methodAliases;
 	
 	/**
 	 * Inits the InputHandler with an owner.
@@ -18,6 +34,19 @@ public class InputHandler extends Thread {
 	 */
 	public InputHandler(Console p_owner) {
 		setOwner(p_owner);
+		d_methodAliases = new HashMap<>();
+		// TODO: transfer this to the main console class?
+		if (getOwner() != null && getOwner().getController() != null) {
+			Class l_controllerClass = d_owner.getController().getClass();
+			if (l_controllerClass != null) {
+				d_controllerMethods = l_controllerClass.getDeclaredMethods();
+				d_methodAliases.put("assigncountries", "assignterritories");
+				d_methodAliases.put("showmap", "printmap");
+				d_methodAliases.put("finish", "finishorders");
+				d_methodAliases.put("editcountry", "editterritory");
+				d_methodAliases.put("editneighbor", "editneighbour");
+			}
+		}
 	}
 	
 	/**
@@ -53,61 +82,12 @@ public class InputHandler extends Thread {
 				String[] l_splitInput = l_input.split(" ");
 				
 				if (l_splitInput.length > 0) {
-					
 					boolean l_areParametersInvalid = false;
+
+					// Deal with aliased names before proceeding.
+					String l_convertedInput = d_methodAliases.containsKey(l_splitInput[0]) ? d_methodAliases.get(l_splitInput[0]) : l_splitInput[0];
 					
-					switch(l_splitInput[0].toLowerCase()) {
-					
-						case("quit"):
-							getOwner().getController().quit();
-							return;
-					
-						case("loadmap"):
-							if (l_splitInput.length != 2) {
-								l_areParametersInvalid = true;
-							}
-							else {
-						 		getOwner().getController().loadMap(l_splitInput[1]);
-							}
-							break;
-							
-						case("savemap"):
-							if (l_splitInput.length != 2) {
-								l_areParametersInvalid = true;
-							}
-							else {
-								getOwner().getController().saveMap(l_splitInput[1]);
-							}
-							break;
-							
-						case("editmap"):
-							if (l_splitInput.length != 2) {
-								l_areParametersInvalid = true;
-							}
-							else {
-								getOwner().getController().editMap(l_splitInput[1]);
-							}
-							break;
-							
-						// TODO: Make the map display different for map editing and playing!!!
-						case("showmap"):
-							if (l_splitInput.length != 1) {
-								l_areParametersInvalid = true;
-							}
-							else {
-								getOwner().getController().printMap();
-							}
-							break;
-							
-						case("validatemap"):
-							if (l_splitInput.length != 1) {
-								l_areParametersInvalid = true;
-							}
-							else {
-								getOwner().getController().validateMap();
-							}
-							break;
-							
+					switch(l_convertedInput.toLowerCase()) {
 						/**
 						 * TODO: Most of these commands below use copy-pasted loops to check parameters due to lack of time.
 						 * These need to be heavily refactored!
@@ -166,7 +146,6 @@ public class InputHandler extends Thread {
 							
 						// TODO: Valid parameters are invalid.
 						case("editterritory"):
-						case("editcountry"):
 							// Input must be at least three words.
 							if (l_splitInput.length <= 2) {
 								l_areParametersInvalid = true;
@@ -219,7 +198,6 @@ public class InputHandler extends Thread {
 							
 						// TODO: Valid parameters are invalid.
 						case("editneighbour"):
-						case("editneighbor"):
 							// Input must be at least three words.
 							if (l_splitInput.length <= 2 || (l_splitInput.length - 1) % 3 != 0) {
 								l_areParametersInvalid = true;
@@ -283,16 +261,6 @@ public class InputHandler extends Thread {
 								}
 							}
 							break;
-						
-						case("assignterritories"):
-						case("assigncountries"):
-							if (l_splitInput.length != 1) {
-								l_areParametersInvalid = true;
-							}
-							else {
-								getOwner().getController().assignTerritories();
-							}
-							break;
 							
 						case("deploy"):
 							if (l_splitInput.length != 3) {
@@ -321,13 +289,52 @@ public class InputHandler extends Thread {
 								}
 							}
 							break;
-							
-						case("finish"):
-							getOwner().getController().finishOrders();
-							break;
 						
 						default:
-							getOwner().onAddMessage("Command \"" + l_splitInput[0] + "\" not recognized.");
+							// Attempt to find and call the method via reflection. Currently only supports methods with no parameters or one string parameter.
+							boolean l_wasMethodFound = false;
+							for (Method l_method : d_controllerMethods) {
+								// If we found the method, go ahead and attempt to call it.
+								if (l_method.getName().equalsIgnoreCase(l_convertedInput)) {
+									l_wasMethodFound = true;
+									int l_numInputParams = l_splitInput.length - 1;
+									int l_numMethodParams = l_method.getParameterCount();
+									// Only continue if the number of parameters match.
+									if (l_numInputParams == l_numMethodParams) {
+										try {
+											// Only call the method if it takes no parameters or it takes just one string.
+											if (l_numInputParams == 1 && l_method.getParameterTypes()[0].equals(String.class)) {
+												l_method.invoke(getOwner().getController(), l_splitInput[1]);
+											}
+											else if (l_numInputParams == 0) {
+												l_method.invoke(getOwner().getController(), null);
+											}
+											else {
+												getOwner().onAddMessage("Command \"" + l_splitInput[0] + "\" cannot currently be used via the method reflection system. Please implement it manually.");
+											}
+										} 
+										catch (IllegalAccessException l_exception) {
+											// TODO Auto-generated catch block
+											l_exception.printStackTrace();
+										} 
+										catch (IllegalArgumentException l_exception) {
+											l_areParametersInvalid = true;
+										} 
+										catch (InvocationTargetException l_exception) {
+											// TODO Auto-generated catch block
+											l_exception.printStackTrace();
+										}
+									}
+									else {
+										l_areParametersInvalid = true;
+									}
+									break;
+								}
+							}
+							// If the method was not found, go ahead and note that it was not recognized.
+							if (!l_wasMethodFound) {
+								getOwner().onAddMessage("Command \"" + l_splitInput[0] + "\" not recognized.");
+							}
 					}
 					
 					if (l_areParametersInvalid) {
@@ -337,9 +344,9 @@ public class InputHandler extends Thread {
 
 			}
 		} 
-        catch (IOException e) {
+        catch (IOException l_exception) {
 			// Just print error and go continue to the exit portion if we encounter an error.
-			getOwner().onAddMessage("Terminating program due to IOException: " + e.getMessage());
+			getOwner().onAddMessage("Terminating program due to IOException: " + l_exception.getMessage());
 		}
 	}
 }
