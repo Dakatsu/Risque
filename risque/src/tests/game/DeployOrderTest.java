4796a72cc8ce42f0854510dbe102a94de10b6ab1
Mansajan <mansajan1997@gmail.com>
2021-04-10 17:32:38 +0530

    test case for deploy order

--- risque/src/tests/game/DeployOrderTest.java
+++ risque/src/tests/game/DeployOrderTest.java
@@ -4,15 +4,22 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import main.game.AdvanceOrder;
+import main.game.DeployOrder;
+
 /**
  * Tests the {@link main.game.DeployOrder} class.
  */
 public class DeployOrderTest {
 
+	/**
+	 * Simple test to ensure we can instantiate this class.
+	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testInstantiateDeployOrder() {
+		int l_numArmies = 0;
+		DeployOrder l_deployOrder = new DeployOrder(null, l_numArmies);
+		assertNotNull(l_deployOrder);
 	}
-
 }
- 
+  
