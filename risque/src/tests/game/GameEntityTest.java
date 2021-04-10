f6694147d7bd44e1cdbbc867284ebe526f02330f
Mansajan <mansajan1997@gmail.com>
2021-04-10 17:55:15 +0530

    test case for game entity class

--- risque/src/tests/game/GameEntityTest.java
+++ risque/src/tests/game/GameEntityTest.java
@@ -4,14 +4,21 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import main.game.DeployOrder;
+import main.game.GameEntity;
+
 /**
  * Tests the {@link main.game.GameEntity} class.
  */
 public class GameEntityTest {
 
+	/**
+	 * Simple test to ensure we can instantiate this class.
+	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGameEntityNotNull() {
+		GameEntity l_GameEntity = new GameEntity(null);
+		assertNotNull(l_GameEntity);
 	}
-
 }
+ 
