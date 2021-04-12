af1488cf030d8a7abad9f10811215192e5ee772d
Mansajan <mansajan1997@gmail.com>
2021-04-13 00:14:47 +0530

    tests

--- risque/src/tests/game/PlayerTest.java
+++ risque/src/tests/game/PlayerTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import main.game.Player;
+import main.game.Territory;
 
 /**
  * Tests the {@link main.game.Player} class.
@@ -40,5 +41,41 @@ public class PlayerTest {
 		String l_name = "Sajan";
 		Player l_player = new Player(l_name);
 		assertNotNull(l_player);
+	}
+	
+	/**
+	 * Tests addOwnedTerritory() function of Player class.
+	 */
+	@Test
+	public void addOwnedTerritoryTest() {
+		Territory l_territory = new Territory("Ontario");
+		Player l_player = new Player("Europe");
+		assertEquals(l_player.addOwnedTerritory(l_territory), true);
+		assertEquals(l_player.addOwnedTerritory(l_territory), false);	
+	}
+	
+	/**
+	 * Tests removeOwnedTerritory() function of Player class.
+	 */
+	@Test
+	public void removeOwnedTerritoryTest() {
+		Territory l_territory = new Territory("Vancouver");
+		Player l_player = new Player("Europe");
+		l_player.addOwnedTerritory(l_territory);
+		assertEquals(l_player.removeOwnedTerritory(l_territory), true);
+		assertEquals(l_player.removeOwnedTerritory(l_territory), false);	
+	}
+	
+	/**
+	 * Tests getNumTerritoriesOwned() function of Player class.
+	 */
+	@Test
+	public void getNumTerritoriesOwnedTest() {
+		Territory l_territory = new Territory("Vancouver");
+		Territory l_territory2 = new Territory("Ontario");
+		Player l_player = new Player("Europe");
+		l_player.addOwnedTerritory(l_territory); 
+		l_player.addOwnedTerritory(l_territory2);
+		assertEquals(l_player.getNumTerritoriesOwned(), 2);	
 	}
 }
